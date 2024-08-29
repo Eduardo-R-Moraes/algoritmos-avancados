867222590ef9956754a4ebaa2b078e13c8e41959
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-08-29 13:22:47 -0300

    first commit

---
+++ 3° aula/lista de adjacencias/tratamento_erros.py
@@ -0,0 +1,44 @@
+###
+# Funções para tratamento de erros
+# ###
+ 
+def pegar_vertice():
+    while True:
+        try:
+            print('Insira um número positivo')
+            vertices = int(input('|V|: '))
+            if vertices > 0:
+                return vertices
+            print()
+        except ValueError:
+            print()
+
+def pegar_origem():
+    while True:
+        try:
+            origem = int(input('\nVértice de origem: '))
+            if origem > 0:
+                return origem
+            print()
+        except ValueError:
+            print()
+            
+def pegar_destino():
+    while True:
+        try:
+            destino = int(input('Vértice de destino: '))
+            if destino > 0:
+                return destino
+            print()
+        except ValueError:
+            print()
+
+def pegar_peso():
+    while True:
+        try:
+            peso = int(input('Peso da aresta (se não tiver pressione <ENTER>): '))
+            if peso > 0:
+                return peso
+            print()
+        except ValueError:
+            print()
---
+++ 3° aula/matriz de adjacencias/main.py
@@ -0,0 +1,11 @@
+from Grafo import Grafo
+
+grafo = Grafo(6, True)
+
+grafo.mostrar()
+
+print()
+
+grafo.adicionar(1, 6, -1)
+
+grafo.mostrar()
