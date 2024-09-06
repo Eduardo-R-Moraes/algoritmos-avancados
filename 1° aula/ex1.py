5c085349be2303f50700e7bc126b966f730a7870
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-09-06 16:13:32 -0300

    Create ex1.py

---
+++ 1° aula/ex1.py
@@ -0,0 +1,16 @@
+'''
+Canibais e missionários: Três missionários e três canibais vão atravessar de uma margem para a outra de um rio, usando um barco onde só cabem duas pessoas de cada vez. Os missionários têm que ser cautelosos para que os canibais nunca os excedam em número em nenhuma das margens do rio. Como poderão passar todos para a outra margem, usando apenas aquele barco? Escreva um algoritmo que demonstre os passos necessários para solucionar este problema.
+
+Resposta:
+    1. Dois canibais atravessam;
+    2. Um canibal volta;
+    3. Dois canibais atravessam;
+    4. Um canibal volta;
+    5. Dois missionários atravessam;
+    6. Um canibal e um missionário volta;
+    7. Dois missionários atravessam;
+    8. Um canibal volta;
+    9. Dois canibais atravessam;
+    10. Um canibal volta;
+    11. Dois canibais atravessam;
+'''
