010a2f4c1fc3470748c95c0a00b294112f1ad1b6
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-10-17 17:04:18 -0300

    11° aula

--- 11° aula/memorizacao.py
+++ 11° aula/memorizacao.py
@@ -1,37 +1,33 @@
-'''
-# Esta nada mais é do que uma sequência de fibonacci com recursividade normal
-  com a adição da tabela chamada aux.
+class Fibonacci:
+    def __init__(self):
+        self.fibonacci = []
 
-# Quando se calcula o fibonacci normal, a cada chamada da função ele recalcula
-  a sequencia inteira. Com a adição do vetor aux, a sequência precisa ser 
-  calculada apenas uma vez, sendo que, a cada iteração é usado os valores pré
-  calculados.
+    def retornarFibo(self, n):
+        if n > len(self.fibonacci):
+            for i in range(n + 1):
+                self.fibonacci.append(False)
 
-# Para fazer a implementação, foi necessário adicionar uma condição a mais
+            self.fiboAlonga(n)
 
-# Ela tem duas condições de paradas, uma para chegar no final do vetor e
-  outra pra quando o vetor já estiver populado
-'''
+        return self.fibonacci[n]
 
-def fibonacciMemorização(n):
-    visitado = [False] * (n+1)
-    return lookFibo(visitado, n)
+    def fiboAlonga(self, n):
+        # Se a opção já foi visitada retorna o valor
+        if self.fibonacci[n]:
+            return self.fibonacci[n]
+        
+        # adiciona aos primeiros indices o valor 0 e 1
+        if n == 0 or n == 1:
+            self.fibonacci[n] = n
+        
+        else: 
+            # Aqui impede que haja repetição caso o resultado seja 0 ou 1
+            self.fibonacci[n] = self.fiboAlonga(n-1) + self.fiboAlonga(n-2)
 
-def lookFibo(visitado, n):
-    # Se a opção já foi visitada retorna o valor
-    if visitado[n]:
-        return visitado[n]
-    
-    # adiciona aos primeiros indices o valor 0 e 1
-    if n == 0 or n == 1:
-        visitado[n] = n
-    
-    else: 
-        # Aqui impede que haja repetição caso o resultado seja 0 ou 1
-        visitado[n] = lookFibo(visitado, n-1) + lookFibo(visitado, n-2)
+        # é retornado no final de todas as chamadas e retorna primeiro 1 e depois 0
+        return self.fibonacci[n]
 
-    # é retornado no final de todas as chamadas e retorna primeiro 1 e depois 0
-    return visitado[n]
+fibonacci = Fibonacci()
 
 posição = int(input('Digite a posição do fibonacci: '))
-print(f'Número fibonacci na posição {posição}: {fibonacciMemorização(posição)}')
+print(f'Número fibonacci na posição {posição}: {fibonacci.retornarFibo(posição)}')
