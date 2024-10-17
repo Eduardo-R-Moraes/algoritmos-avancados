353d78d2d41d4f68135baa2e181ae162e4563201
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-10-17 15:39:13 -0300

    11° aula

--- 11° aula/memorizacao.py
+++ 11° aula/memorizacao.py
@@ -1,19 +1,37 @@
-def fibonacciMemorização(n):
-    aux = [-1] * (n+1)
-    lookFibo(aux, n)
-    return aux[n-1] + aux[n-2]
+'''
+# Esta nada mais é do que uma sequência de fibonacci com recursividade normal
+  com a adição da tabela chamada aux.
 
-def lookFibo(aux, n):
-    if aux[n] >= 0:
-        return aux[n]
+# Quando se calcula o fibonacci normal, a cada chamada da função ele recalcula
+  a sequencia inteira. Com a adição do vetor aux, a sequência precisa ser 
+  calculada apenas uma vez, sendo que, a cada iteração é usado os valores pré
+  calculados.
+
+# Para fazer a implementação, foi necessário adicionar uma condição a mais
+
+# Ela tem duas condições de paradas, uma para chegar no final do vetor e
+  outra pra quando o vetor já estiver populado
+'''
+
+def fibonacciMemorização(n):
+    visitado = [False] * (n+1)
+    return lookFibo(visitado, n)
+
+def lookFibo(visitado, n):
+    # Se a opção já foi visitada retorna o valor
+    if visitado[n]:
+        return visitado[n]
     
-    if n <= 1:
-        aux[n] = n
+    # adiciona aos primeiros indices o valor 0 e 1
+    if n == 0 or n == 1:
+        visitado[n] = n
     
     else: 
-        aux[n] = lookFibo(aux, n-1) + lookFibo(aux, n-2)
+        # Aqui impede que haja repetição caso o resultado seja 0 ou 1
+        visitado[n] = lookFibo(visitado, n-1) + lookFibo(visitado, n-2)
 
-    return aux[n]
+    # é retornado no final de todas as chamadas e retorna primeiro 1 e depois 0
+    return visitado[n]
 
 posição = int(input('Digite a posição do fibonacci: '))
 print(f'Número fibonacci na posição {posição}: {fibonacciMemorização(posição)}')
