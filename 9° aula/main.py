2749066511b0f3d6101d83e43284b68c6c930a6c
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-10-18 07:37:23 -0300

    9° aula

--- 9° aula/main.py
+++ 9° aula/main.py
@@ -1,43 +1,47 @@
-dx = [2, 1, -1, -2, -2, -1, 1, 2]
-dy = [1, 2, 2, 1, -1, -2, -2, -1]
+class PasseioCavalo:
+    def __init__(self, num):
+        self.dx = [2, 1, -1, -2, -2, -1, 1, 2]
+        self.dy = [1, 2, 2, 1, -1, -2, -2, -1]
+        self.num = num
+        self.numeroQuadrados = num ** 2
+        self.tab = [[0 for i in range(num)] for i in range(num)]
 
-def aceitavel(x, y):
-    return 0 <= x < num and 0 <= y < num and tab[x][y] == 0
+    def aceitavel(self, x, y):
+        return 0 <= x < self.num and 0 <= y < self.num and self.tab[x][y] == 0
 
-def tenta(qtde_visitas_feitas, x, y):
-    if qtde_visitas_feitas == numQuad: 
-        return True
+    def tenta(self, qtde_visitas_feitas, x, y):
+        if qtde_visitas_feitas == self.numeroQuadrados: 
+            return True
 
-    for k in range(8):
-        u = x + dx[k]
-        v = y + dy[k]
+        for k in range(8):
+            u = x + self.dx[k]
+            v = y + self.dy[k]
 
-        if aceitavel(u, v):
-            tab[u][v] = qtde_visitas_feitas
+            if self.aceitavel(u, v):
+                self.tab[u][v] = qtde_visitas_feitas
 
-            if tenta(qtde_visitas_feitas + 1, u, v):
-                return True 
+                if self.tenta(qtde_visitas_feitas + 1, u, v):
+                    return True 
 
-            tab[u][v] = 0 
+                self.tab[u][v] = 0 
 
-    return False 
+        return False 
 
-def printTabuleiro():
-    for i in range(num):
-        for j in range(num):
-            print(f"{tab[i][j]:2}", end="\t")  
-        print("")
+    def printTabuleiro(self):
+        for i in range(self.num):
+            for j in range(self.num):
+                print(f"{self.tab[i][j]:2}", end="\t")  
+            print("")
 
-def passeia(x, y):
-    tab[x][y] = 1  
-    if tenta(2, x, y):  
-        printTabuleiro()
-    else:
-        print("Não existem passeios possíveis para o tabuleiro informado.")
+    def passeia(self, x, y):
+        self.tab[x][y] = 1  
+        if self.tenta(2, x, y):  
+            self.printTabuleiro()
+        else:
+            print("Não existem passeios possíveis para o tabuleiro informado.")
 
 num = int(input("Informe o tamanho do tabuleiro. Exemplo: 8 para 8x8: "))
-numQuad = num * num
 
-tab = [[0] * num for i in range(num)]
+cavalo = PasseioCavalo(num)
 
-passeia(0, 0)
+cavalo.passeia(0, 0)
